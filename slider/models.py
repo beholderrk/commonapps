d4cbdf5b4e12af618fd4b73fc06c7d92789b0ae7
sim <simonov.alexey@gmail.com>
2012-02-06 14:58:28 +0400

    add several sliders in settings

--- slider/models.py
+++ slider/models.py
@@ -3,14 +3,23 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from sorl.thumbnail.fields import ImageField
+from django.conf import settings
 
 class Slider(models.Model):
     """ Слайдер
     """
+
+    try:
+        SHOW_ON_CHOICES = settings.SLIDER_SHOW_ON_CHOICES
+    except Exception as e:
+        SHOW_ON_CHOICES = (
+            ("main", u"главная"),
+        )
+
     title = models.CharField(_(u'заголовок'), blank=True, max_length=250, help_text=_(u'Рабочее название для админки.'))
     show_on = models.CharField(_(u'показывать на'), default=u'main', max_length=250,
                                help_text=_(u'принадлежность к странице, на которой будет находиться слайд'),
-                               choices=(("main", "главной странице (982×430)"),))
+                               choices=SHOW_ON_CHOICES)
     image = ImageField(_(u'изображение'), upload_to='slider')
     name = models.CharField(_(u'название'), blank=True, max_length=250, help_text=_(u'будет показываться при наведении на слайд; максимум 250 символов.'))
     url = models.CharField(_(u'ссылка'), blank=True, max_length=10000)
