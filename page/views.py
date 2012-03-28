b5f2250a70a282f1cc17de2d20e812437a91693e
sim <simonov.alexey@gmail.com>
2012-03-28 18:37:35 +0400

    clear related name for PageBlock

--- page/views.py
+++ page/views.py
@@ -10,7 +10,7 @@ def page_view(request, slug):
     """Displays page with passed slug
     """
     page = get_object_or_404(Page, slug=slug)
-    parts = page.pages_containing.filter(active = True)
+    parts = page.page_blocks.filter(active = True)
     if request.user.is_superuser or page.active:
         return { 'TEMPLATE': page.template_name, 'page': page, 'parts': parts }
     raise Http404('No %s matches the given query.' % page._meta.object_name)
