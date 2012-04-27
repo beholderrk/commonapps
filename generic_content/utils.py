5ff938496726a633bb586f0e56574468ed4bf4c1
sim <simonov.alexey@gmail.com>
2012-04-27 17:23:56 +0400

    bugfix in clear_internal_url

--- generic_content/utils.py
+++ generic_content/utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.core.urlresolvers import reverse, resolve, Resolver404
 from urlparse import urlparse
+from django.contrib.sites.models import Site
 
 try:
     from localeurl.utils import strip_path as locale_strip_path
@@ -9,8 +10,12 @@ except ImportError:
         return '', path
 
 def clear_internal_url(url, return_reverse=False):
-    """Внутренняя ссылка возвращается без локали, как относительная, внешняя ссылка не изменяется"""
+    """ Внутренняя ссылка возвращается без локали, как относительная, внешняя ссылка не изменяется
+        Нужен правильно указанный в админке домен сайта """
+    current_site = Site.objects.get_current()
     link = urlparse(url)
+    if link.netloc != current_site.domain:
+        return url
     path = link.path + ('#' + link.fragment if link.fragment else '') + ('?' + link.query if link.query else '')
     try:
         path = locale_strip_path(path)[1] # удаление локали
