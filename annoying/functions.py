fbedd16a84173188e07946c89c5a1ae9b211df22
beholderrk <beholderrk@gmail.com>
2012-11-16 16:34:08 +0400

    get_first_or_None

--- annoying/functions.py
+++ annoying/functions.py
@@ -1,6 +1,6 @@
 from django.shortcuts import _get_queryset
 from django.conf import settings
-
+from django.db.models.query import QuerySet
 
 def get_object_or_None(klass, *args, **kwargs):
     """
@@ -18,7 +18,24 @@ def get_object_or_None(klass, *args, **kwargs):
     except queryset.model.DoesNotExist:
         return None
 
+def get_first_or_None(qsinit, *args, **kwargs):
+    if not qsinit:
+        return None
 
+    if isinstance(qsinit, list):
+        try:
+            return qsinit[0]
+        except IndexError:
+            return None
+
+    if isinstance(qsinit, QuerySet):
+        queryset = qsinit
+    elif isinstance(qsinit, type):
+        queryset = _get_queryset(qsinit)
+    try:
+        return queryset.filter(*args, **kwargs)[0]
+    except IndexError:
+        return None
 
 def get_config(key, default):
     """
